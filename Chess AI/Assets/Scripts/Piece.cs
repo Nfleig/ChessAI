474bcc3f8886b4cb5bd3b3ded14106cbe3942e37
Nfleig <65872728+Nfleig@users.noreply.github.com>
2021-10-20 15:56:37 -0400

    Made the controls more consistent on mobile

--- Chess AI/Assets/Scripts/Piece.cs
+++ Chess AI/Assets/Scripts/Piece.cs
@@ -46,6 +46,7 @@ public class Piece : MonoBehaviour
                 {
                     marker.SetActive(false);
                 }
+                showingMoves = false;
             }
             wasClicked = false;
         }
